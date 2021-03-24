72a705476b10e9744fdd1670cb5801c22c60ddfa
JFM <jeanfrancois.morin@wildcodeschool.com>
2021-03-24 16:11:58 +0100

    add class active to flib-box + css stuffs

--- script_menu.js
+++ script_menu.js
@@ -1,5 +1,16 @@
+let btnFlipp = document.getElementsByClassName('btn-flip-card');
+
+for (let i = 0; i < btnFlipp.length; i++){
+    let element = btnFlipp[i];
+    element.addEventListener("click", function(elem){
+        let parent = elem.target.closest(".flip-box")
+        parent.classList.toggle("active")
+    })
+}
+
+
 function TournPage() {
-    document.getElementsByClassName("flip-box")[0].classList.toggle("active");
+    // document.getElementsByClassName("flip-box")[0].classList.toggle("active");
     // document.getElementById("flip-box-front").style.transform.style = "preserve-3d";
 
     // document.getElementById("flip-box-back").style.transform = "rotateY(0deg)";
