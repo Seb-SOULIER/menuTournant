c3528db32d33a91be8f99c069c00561ce9bc9e79
Seb-SOULIER <soulier.sebastien.ss@gmail.com>
2021-03-24 09:12:25 +0100

    Menu tournant

---
+++ script_menu.js
@@ -0,0 +1,18 @@
+function TournPage() {
+    document.getElementsByClassName("flip-box")[0].classList.toggle("active");
+    // document.getElementById("flip-box-front").style.transform.style = "preserve-3d";
+
+    // document.getElementById("flip-box-back").style.transform = "rotateY(0deg)";
+    // document.getElementById("flip-box-back").style.transform.style = "preserve-3d";
+
+    }
+
+//   .flip-box:hover .flip-box-front {
+//     transform: rotateY(-180deg);
+//     transform-style: preserve-3d;
+//   }
+
+//   .flip-box:hover .flip-box-back {
+//     transform: rotateY(0deg);
+//     transform-style: preserve-3d;
+//   }
